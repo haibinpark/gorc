30ef06e4f0c68ef7601bc91a4e99e63614669e91
haibinpark <zhaohaibin007@gmail.com>
2013-09-25 17:39:56 +0800

    rename install-vimrc.sh to install-gorc.sh

---
+++ install-gorc.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# install vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
+#install zshrc
+ln -s ~/.vim/zshrc ~/.zshrc
+#install ctags for golang
+ln -s ~/.vim/ctags ~/.ctags
+
--- install-vimrc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-ln -s ~/.vim/vimrc ~/.vimrc
