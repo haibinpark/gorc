5c53a592cad1c9099e015fda0ae67e1523582a6e
haibinpark <zhaohaibin007@gmail.com>
2013-11-14 23:45:21 +0800

    remove ctags symbole link

--- install-gorc.sh
+++ install-gorc.sh
@@ -3,8 +3,7 @@
 ln -sf ~/.vim/vimrc ~/.vimrc
 #install zshrc
 ln -sf ~/.vim/zshrc ~/.zshrc
-#install ctags for golang
-#ln -sf ~/.vim/ctags ~/.ctags
+
 #config tmux.conf
 ln -sf ~/.vim/tmuxrc ~/.tmux.conf
 
