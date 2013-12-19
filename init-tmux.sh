842ee051c5bff757f8e7220d32054fdb63e8ae99
haibinpark <zhaohaibin007@gmail.com>
2013-12-19 09:39:22 +0800

    fixbug:can't new seesion and windows

--- init-tmux.sh
+++ init-tmux.sh
@@ -17,13 +17,13 @@ fi
 $cmd has -t $session 2> /dev/null
 
 if [ $? != 0 ]; then
-    $cmd new -d -n vim -s $session "vim"
-    #$cmd neww -n mutt -t $session "mutt"
-    $cmd neww -n irssi -t $session "irssi"
-    $cmd neww -n cmus -t $session "cmus"
-    $cmd neww -n go-dev -t $session "go-dev"
-    #$cmd splitw -h -p 50 -t $session "zsh"
-    $cmd selectw -t $session:4
+    $cmd new -d -n dev -s $session 
+    $cmd neww -n debug -t $session
+    $cmd neww -n run -t $session
+    $cmd neww -n db -t $session 
+    $cmd neww -n file -t $session
+    $cmd neww -n git -t $session
+    $cmd selectw -t $session:1
 fi
 
 $cmd att -t $session
